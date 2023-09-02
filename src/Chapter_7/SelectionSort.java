afe524ca5945406222fd6168f70bfc6a0ab8ac85
Abdul Wasay Imran <55891247+awasay905@users.noreply.github.com>
2023-09-02 11:16:09 +0500

    Completed Chapter 7 Book Examples

---
+++ src/Chapter_7/SelectionSort.java
@@ -0,0 +1,24 @@
+package Chapter_7;
+
+public class SelectionSort {
+
+    public static void selectionSort(double[] list){
+        for (int i = 0; i < list.length -1; i++) {
+
+            double currentMin = list[i];
+            int currentMinIndex = i;
+
+            for (int j = 1+i; j < list.length; j++) {
+                if (currentMin > list[j]){
+                    currentMin = list[j];
+                    currentMinIndex = j;
+                }
+            }
+
+            if (currentMinIndex != i){
+                list[currentMinIndex] = list[i];
+                list[i] = currentMin;
+            }
+        }
+    }
+}
